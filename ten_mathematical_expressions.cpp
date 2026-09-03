28e5b4cde7bfa8b5653b2335e4cfe4df49e932a4
Molly <himollyh4@gmail.com>
2026-09-03 09:21:33 -0400

    Fisnished set 0

---
+++ ten_mathematical_expressions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n = 5;
+	int m = 7;
+	cout << "The product of " << n << " and " << m << " is ";
+	cout << n * m << "." << endl;
+	
+	return 0;
+}
