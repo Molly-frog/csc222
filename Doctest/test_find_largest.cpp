b9a0a0e2f845167b654e4ed76c0d4973299d6f9b
Molly <himollyh4@gmail.com>
2026-09-24 09:00:14 -0400

    added find largetst number...FAILED

---
+++ Doctest/test_find_largest.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+using namespace std;
+
+int find_largest(int x, int y) {
+    if (x > y) { 
+        return x;
+    } else {
+        return x;
+    }
+}
+
+TEST_CASE("find_largest returns the greater of two integers") {
+    CHECK(find_largest(6, 19) == 19);
+    CHECK(find_largest(6, 1) == 6);
+    CHECK(find_largest(22, 42) == 42);
+    CHECK(find_largest(42, 42) == 42);
+}
