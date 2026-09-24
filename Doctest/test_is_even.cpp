0b9b2f4f3b381d77325ca8a5121777f4a828538f
Molly <himollyh4@gmail.com>
2026-09-24 08:50:51 -0400

    added test_even

---
+++ Doctest/test_is_even.cpp
@@ -0,0 +1,16 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+using namespace std;
+
+bool is_even(int n) {
+    return n % 2 == 0;
+}
+
+
+TEST_CASE("is_even identifies even numbers") {
+    CHECK(is_even(0) == true);
+    CHECK(is_even(2) == true);
+    CHECK(is_even(-4) == true);
+    CHECK(is_even(1) == false);
+    CHECK(is_even(-7) == false);
+}
